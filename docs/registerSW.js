7ff01cd5525dbfceba69cbe180efc68657cf4aa3
Andrew Manzanero <andrewmanzanero@proton.me>
2024-11-24 12:37:26 -0800

    pwa-ify

---
+++ docs/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tracking-account-updater-for-ynab/sw.js', { scope: '/tracking-account-updater-for-ynab/' })})}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.png"],
+      manifest: {
+        name: "Tracking Account Updater For Ynab",
+        short_name: "TrackingUpdater",
+        description: "Connect to YNAB and update your tracking accounts in bulk",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
